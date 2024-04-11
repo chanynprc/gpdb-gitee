e6ff22e87443fc4c0faec17021c3c131d600abed
Sasasu <i@sasa.su>
2024-04-11 16:20:14 +0800

    add basebackup_file_read_buffer_modify_hook
    basebackup will read the file directly, adding a new hook to process the reared buffer

--- src/include/replication/basebackup.h
+++ src/include/replication/basebackup.h
@@ -33,4 +33,18 @@ extern void SendBaseBackup(BaseBackupCmd *cmd);
 
 extern int64 sendTablespace(char *path, bool sizeonly);
 
+/*
+ * The hook to modify the read buffer in basebackup
+ *
+ * Arguments:
+ *	fname: the file name, relative path to DataDir
+ *	blkno: the block number, starts from 0 in the current segment
+ *	buf:   the buffer needs to be modified
+ *
+ * Returns the new buffer after modified or the original buffer if no need to modify
+ * The caller does not need to free the new buffer.
+ */
+typedef char* (*basebackup_file_before_verify_hook_type)(const char *fname, int blkno, char *buf);
+extern PGDLLIMPORT basebackup_file_before_verify_hook_type basebackup_file_before_verify_hook;
+
 #endif							/* _BASEBACKUP_H */
